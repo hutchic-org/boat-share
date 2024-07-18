6c02fd3049aa090cbea055461bdafe5734733f86
Colin Hutchinson <chutchic@gmail.com>
2024-07-18 16:35:40 -0400

    feat(oauth): ability to oauth against Google / Google Calendar

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,29 @@
 <template>
   <UContainer>
-    <VitePwaManifest />
+    <NuxtPwaAssets />
     <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme>
-            <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" />
-          </ColorScheme>
-        </div>
-      </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
+      <UButton @click="login">Login with Google</UButton>
     </UCard>
   </UContainer>
 </template>
+
+<script setup lang="ts">
+import { useTokenClient, type AuthCodeFlowSuccessResponse, type AuthCodeFlowErrorResponse } from 'vue3-google-signin';
+
+const handleOnSuccess = (response: AuthCodeFlowSuccessResponse) => {
+  console.log('Access Token: ', response.access_token);
+  // Use the access token to interact with Google Calendar API
+};
+
+const handleOnError = (errorResponse: AuthCodeFlowErrorResponse) => {
+  console.log('Error: ', errorResponse);
+};
+
+const { login } = useTokenClient({
+  client_id: '753759858538-nufcl1qbhf9gpc7v9qs4seramd7ni2rm.apps.googleusercontent.com',
+  scope: 'profile email https://www.googleapis.com/auth/calendar',
+  prompt: 'consent',
+  onSuccess: handleOnSuccess,
+  onError: handleOnError,
+});
+</script>
