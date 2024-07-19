2ba824161651c010415c5b7c55610ca13f6357fd
Colin Hutchinson <chutchic@gmail.com>
2024-07-19 09:50:45 -0400

    fix(pwa): make the icons relatively pathed

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -18,17 +18,17 @@ export default defineNuxtConfig({
       theme_color: '#4DBA87',
       icons: [
         {
-          src: '/icons/50.png',
+          src: 'icons/50.png',
           sizes: '50x50',
           type: 'image/png'
         },
         {
-          src: '/icons/192.png',
+          src: 'icons/192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: '/icons/512.png',
+          src: 'icons/512.png',
           sizes: '512x512',
           type: 'image/png'
         },
