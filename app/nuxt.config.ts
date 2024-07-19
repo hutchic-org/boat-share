6db99ba4dd2367219dcd22b1b5396a331cb5513a
Colin Hutchinson <chutchic@gmail.com>
2024-07-19 10:38:49 -0400

    fix(dev): make sure local development continues to work

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -13,30 +13,30 @@ export default defineNuxtConfig({
       description: 'An app for sharing a boat among a pre-defined set of users via a Google Calendar backend.',
       lang: 'en',
       display: 'standalone',
-      scope: '/boat-share/',
-      start_url: '/boat-share/',
+      scope: process.env.BASE_URL || '/',
+      start_url: process.env.BASE_URL || '/',
       background_color: '#ffffff',
       theme_color: '#4DBA87',
       icons: [
         {
-          src: '/boat-share/icons/50.png',
+          src: `${process.env.BASE_URL || ''}/icons/50.png`,
           sizes: '50x50',
           type: 'image/png'
         },
         {
-          src: '/boat-share/icons/192.png',
+          src: `${process.env.BASE_URL || ''}/icons/192.png`,
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: '/boat-share/icons/512.png',
+          src: `${process.env.BASE_URL || ''}/icons/512.png`,
           sizes: '512x512',
           type: 'image/png'
         },
       ]
     },
     workbox: {
-      navigateFallback: 'https://hutchic-org.github.io/boat-share/'
+      navigateFallback: `${process.env.BASE_URL || ''}/`
     },
     client: {
       installPrompt: true
@@ -56,6 +56,9 @@ export default defineNuxtConfig({
         clientPort: 3000
       }
     }
+  },
+  app: {
+    baseURL: process.env.BASE_URL || '/',
   },
   compatibilityDate: '2024-07-03',
 })
