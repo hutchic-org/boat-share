42012b1343fe0ed3d2503e530346b9ce9ab51b55
Colin Hutchinson <chutchic@gmail.com>
2024-07-19 09:55:41 -0400

    fix(styles): the icon change broke the style?

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -16,26 +16,23 @@ export default defineNuxtConfig({
       start_url: '/',
       background_color: '#ffffff',
       theme_color: '#4DBA87',
-      icons: [
-        {
-          src: 'icons/50.png',
-          sizes: '50x50',
-          type: 'image/png'
-        },
-        {
-          src: 'icons/192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'icons/512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        },
-      ]
-    },
-    workbox: {
-      navigateFallback: '/'
+      // icons: [
+      //   {
+      //     src: 'icons/50.png',
+      //     sizes: '50x50',
+      //     type: 'image/png'
+      //   },
+      //   {
+      //     src: 'icons/192.png',
+      //     sizes: '192x192',
+      //     type: 'image/png'
+      //   },
+      //   {
+      //     src: 'icons/512.png',
+      //     sizes: '512x512',
+      //     type: 'image/png'
+      //   },
+      // ]
     },
     client: {
       installPrompt: true
