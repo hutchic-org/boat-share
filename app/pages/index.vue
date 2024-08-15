a5238a26160c0b30d22a877e8c7c5aaff7b85903
Colin Hutchinson <hutchic@users.noreply.github.com>
2024-08-15 14:45:15 -0400

    feat(extra-days): display +/- a month (#33)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
         <UButton @click="login">Login with Google</UButton>
       </UCard>
     </UContainer>
-  
+
     <UModal v-model="showInstallPrompt" prevent-close>
       <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
         <template #header>
@@ -24,27 +24,27 @@
       </UCard>
     </UModal>
   </template>
-  
+
   <script setup lang="ts">
   import { ref, onMounted } from 'vue';
   import { useTokenClient, type AuthCodeFlowSuccessResponse, type AuthCodeFlowErrorResponse } from 'vue3-google-signin';
   import { useRouter } from 'vue-router';
   import { useNuxtApp } from '#app';
-  
+
   const router = useRouter();
   const { $pwa } = useNuxtApp();
   const showInstallPrompt = ref(false);
   let deferredPrompt: Event | null;
-  
+
   const handleOnSuccess = (response: AuthCodeFlowSuccessResponse) => {
       sessionStorage.setItem('googleAccessToken', response.access_token);
       router.push('/secure');
   };
-  
+
   const handleOnError = (errorResponse: AuthCodeFlowErrorResponse) => {
       console.log('Error: ', errorResponse);
   };
-  
+
   const { login } = useTokenClient({
       client_id: '753759858538-nufcl1qbhf9gpc7v9qs4seramd7ni2rm.apps.googleusercontent.com',
       scope: 'profile email https://www.googleapis.com/auth/calendar',
@@ -52,12 +52,12 @@
       onSuccess: handleOnSuccess,
       onError: handleOnError,
   });
-  
+
   onMounted(() => {
     // Check if the user is on a mobile device
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     if (!isMobile) return;
-  
+
     // Show the install prompt if it hasn't been dismissed
     if (!localStorage.getItem('installPromptDismissed')) {
       window.addEventListener('beforeinstallprompt', (e) => {
@@ -67,7 +67,7 @@
       });
     }
   });
-  
+
   const installPWA = async () => {
     if (deferredPrompt) {
       deferredPrompt.prompt();
@@ -81,10 +81,9 @@
       showInstallPrompt.value = false;
     }
   };
-  
+
   const dismissInstallPrompt = () => {
     showInstallPrompt.value = false;
     localStorage.setItem('installPromptDismissed', 'true');
   };
   </script>
-  
